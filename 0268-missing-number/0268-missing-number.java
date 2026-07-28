78025af29ddec63454c016aa1da197cca5ab9a8d
Kalpana Narendra Patil <107493464+kalpana23019@users.noreply.github.com>
2026-07-28 22:49:35 +0530

    Time: 0 ms (100%), Space: 47.2 MB (82.73%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        
+
+        int n = nums.length;
+        int expectedSum = n*(n+1)/2;
+
+        int actualsum =0;
+
+        for(int  num : nums){
+            actualsum += num;
+        }
+
+        return expectedSum - actualsum;
+    }
+
+}
