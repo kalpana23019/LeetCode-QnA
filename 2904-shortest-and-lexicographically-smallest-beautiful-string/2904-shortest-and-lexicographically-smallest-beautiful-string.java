8bcaf91b6d792ee823f90b20806ac91bdb1ce3b1
Kalpana Narendra Patil <107493464+kalpana23019@users.noreply.github.com>
2026-08-27 00:21:59 +0530

    Time: 2 ms (40.94%), Space: 44.2 MB (28.86%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -9,10 +9,12 @@ class Solution {
 
         while (right < s.length()) {
 
+            // Add current character to the window
             if (s.charAt(right) == '1') {
                 ones++;
             }
 
+            // Shrink the window
             while (ones > k || (left < right && s.charAt(left) == '0')) {
 
                 if (s.charAt(left) == '1') {
@@ -22,20 +24,25 @@ class Solution {
                 left++;
             }
 
+            // Move right forward
             right++;
 
+            // Check if current window is beautiful
             if (ones == k) {
 
                 String current = s.substring(left, right);
 
+                // First valid answer
                 if (answer.equals("")) {
                     answer = current;
                 }
 
+                // Shorter answer
                 else if (current.length() < answer.length()) {
                     answer = current;
                 }
 
+                // Same length but lexicographically smaller
                 else if (current.length() == answer.length()
                         && current.compareTo(answer) < 0) {
                     answer = current;
