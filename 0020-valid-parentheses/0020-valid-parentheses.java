9564283ebae90aec6ec5a265a05600925725b70d
Kalpana Narendra Patil <107493464+kalpana23019@users.noreply.github.com>
2026-09-01 00:30:56 +0530

    Time: 2 ms (98.08%), Space: 43.2 MB (72.95%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,42 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            // Opening brackets
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            }
+
+            // Closing brackets
+            else {
+
+                // No opening bracket available
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if (ch == ')' && top != '(') {
+                    return false;
+                }
+
+                if (ch == ']' && top != '[') {
+                    return false;
+                }
+
+                if (ch == '}' && top != '{') {
+                    return false;
+                }
+            }
+        }
+
+        // Stack should be empty
+        return stack.isEmpty();
+    }
+}
